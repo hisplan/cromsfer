fbdf983121d50c1a896b9349a78ad626f904a972
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-01-10 12:37:10 -0500

    Add globbed from umiCountMatrix/readCountMatrix

--- src/cromsfer/workflows/Sharp.py
+++ src/cromsfer/workflows/Sharp.py
@@ -5,33 +5,55 @@ import re
 def construct_src_dst_info(workflow_id, outputs, base_destination):
 
     # {
-    #     'Sharp.combinedClass': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HashedCountMatrix/final-classification.tsv.gz',
-    #     'Sharp.combinedCountMatrix': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HashedCountMatrix/final-matrix.tsv.gz',
-    #     'Sharp.combinedLog': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HashedCountMatrix/combine.log',
-    #     'Sharp.combinedStats': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HashedCountMatrix/stats.yml',
-    #     'Sharp.countReport': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-CiteSeqCount/results/run_report.yaml',
-    #     'Sharp.fastQCR1Html': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-FastQCR1/IL10neg_HTO_IGO_10034_23_S2_R1_fastqc.html',
-    #     'Sharp.fastQCR2Html': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-FastQCR2/IL10neg_HTO_IGO_10034_23_S2_R2_fastqc.html',
-    #     'Sharp.htoClassification': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HtoDemuxKMeans/final-classification.tsv.gz',
-    #     'Sharp.htoClassification_Suppl1': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HtoDemuxSeurat/outputs/classification.csv',
-    #     'Sharp.htoClassification_Suppl2': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HtoDemuxSeurat/outputs/full-output.csv',
-    #     'Sharp.htoClassification_Suppl3': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-CorrectFalsePositiveDoublets/final-classification.tsv.gz',
-    #     'Sharp.logHtoDemux': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HtoDemuxKMeans/demux_kmeans.log',
-    #     'Sharp.logHtoDemux_Suppl1': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-CorrectFalsePositiveDoublets/correct_fp_doublets.log',
-    #     'Sharp.statsHtoDemux': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-HtoDemuxKMeans/stats.yml',
-    #     'Sharp.statsHtoDemux_Suppl1': 'gs://chunj-cromwell/cromwell-execution/Sharp/8dad3d5a-59cd-4a45-8b41-43dd16817f2d/call-CorrectFalsePositiveDoublets/stats.yml'
+    # "Sharp.fastQCR1Html": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-FastQCR1/1687_LX33_1_4_1_HTO_R1_fastqc.html",
+    # "Sharp.countReport": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CiteSeqCount/results/run_report.yaml",
+    # "Sharp.logHtoDemux": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HtoDemuxKMeans/demux_kmeans.log",
+    # "Sharp.umiCountMatrix": [
+    #     "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CiteSeqCount/glob-5b2373ebac80816456a7726e786fc4d4/barcodes.tsv.gz",
+    #     "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CiteSeqCount/glob-5b2373ebac80816456a7726e786fc4d4/features.tsv.gz",
+    #     "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CiteSeqCount/glob-5b2373ebac80816456a7726e786fc4d4/matrix.mtx.gz"
+    # ],
+    # "Sharp.readCountMatrix": [
+    #     "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CiteSeqCount/glob-8e7f6a2dd9fb1323e5ebc5c1c063f6df/barcodes.tsv.gz",
+    #     "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CiteSeqCount/glob-8e7f6a2dd9fb1323e5ebc5c1c063f6df/features.tsv.gz",
+    #     "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CiteSeqCount/glob-8e7f6a2dd9fb1323e5ebc5c1c063f6df/matrix.mtx.gz"
+    # ],
+    # "Sharp.logHtoDemux_Suppl1": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CorrectFalsePositiveDoublets/correct_fp_doublets.log",
+    # "Sharp.combinedCountMatrix": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HashedCountMatrix/final-matrix.tsv.gz",
+    # "Sharp.htoClassification_Suppl1": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HtoDemuxSeurat/outputs/classification.csv",
+    # "Sharp.statsHtoDemux": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HtoDemuxKMeans/stats.yml",
+    # "Sharp.statsHtoDemux_Suppl1": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CorrectFalsePositiveDoublets/stats.yml",
+    # "Sharp.combinedLog": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HashedCountMatrix/combine.log",
+    # "Sharp.fastQCR2Html": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-FastQCR2/1687_LX33_1_4_1_HTO_R2_fastqc.html",
+    # "Sharp.combinedStats": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HashedCountMatrix/stats.yml",
+    # "Sharp.htoClassification_Suppl2": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HtoDemuxSeurat/outputs/full-output.csv",
+    # "Sharp.htoClassification": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HtoDemuxKMeans/final-classification.tsv.gz",
+    # "Sharp.combinedClass": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-HashedCountMatrix/final-classification.tsv.gz",
+    # "Sharp.htoClassification_Suppl3": "s3://dp-lab-batch/cromwell-execution/Sharp/b4a27cc5-9b8f-49f8-830e-1a65a477c8c1/call-CorrectFalsePositiveDoublets/final-classification.tsv.gz"
     # }
 
     items = list()
 
     for key in outputs.keys():
 
-        src = outputs[key]
-        match = re.search(workflow_id + "/call-(.*)$", src)
-        if match:
-            items.append(
-                (src, base_destination + "/" + match.group(1))
-            )
+        # is it a list of files from glob? (e.g. umiCountMatrix or readCountMatrix)
+        if isinstance(outputs[key], list):
+            # Sharp.umiCountMatrix --> umiCountMatrix
+            subfolder = key.split(".")[1]
+            for src in outputs[key]:
+                match = re.search(workflow_id + "/call-(.*?)/", src)
+                if match:
+                    items.append(
+                        (src, base_destination + "/" + match.group(1) + "/" + subfolder + "/")
+                    )
+        else:
+            # it's a single file
+            src = outputs[key]
+            match = re.search(workflow_id + "/call-(.*)$", src)
+            if match:
+                items.append(
+                    (src, base_destination + "/" + match.group(1))
+                )
 
     return items
 
@@ -40,6 +62,7 @@ def get_glob_list(workflow_id):
 
     items = [
         "HashedCountMatrix/*",
+        "CiteSeqCount/*",
         "tag-list.csv"
     ]
 
