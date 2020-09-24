33a3daa6c89f8be3d8074f75506918d5f3b7a18b
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-09-23 21:28:40 -0400

    Move Velopipe log files

--- src/cromsfer/workflows/Velopipe.py
+++ src/cromsfer/workflows/Velopipe.py
@@ -4,17 +4,20 @@ import re
 
 def construct_src_dst_info(workflow_id, outputs, base_destination):
 
-    # outLoom
-    # "s3://dp-lab-batch/cromwell-execution/Velopipe/8b57436f-b4eb-4341-9af1-5b9567b40170/call-Velocyto/glob-6e284a9cd30ed4548d4059bf33133003/1380_IL10R_P176_IGO_10084_4_Aligned_DOPRI.loom"
-
     # outCBSortedTaggedBam
-    # "s3://dp-lab-batch/cromwell-execution/Velopipe/8b57436f-b4eb-4341-9af1-5b9567b40170/call-CBSortedTaggedBam/cellsorted_1380_IL10R_P176_IGO_10084_4_Aligned.out.sorted.tagged.bam"
+    # "s3://dp-lab-batch/cromwell-execution/Velopipe2/f8622453-33a1-428b-8eb1-b24c1800214f/call-CBSortedTaggedBam/cellsorted_MPL_CD40.tagged.bam"
 
-    # outPosSortedTaggedBam
-    # "s3://dp-lab-batch/cromwell-execution/Velopipe/8b57436f-b4eb-4341-9af1-5b9567b40170/call-PosSortedTaggedBam/1380_IL10R_P176_IGO_10084_4_Aligned.out.sorted.tagged.sorted.bam"
+    # outLoom
+    # "s3://dp-lab-batch/cromwell-execution/Velopipe2/f8622453-33a1-428b-8eb1-b24c1800214f/call-Velocyto/glob-6e284a9cd30ed4548d4059bf33133003/MPL_CD40_IL001.loom"
 
     # outPosSortedTaggedBai
-    # "s3://dp-lab-batch/cromwell-execution/Velopipe/8b57436f-b4eb-4341-9af1-5b9567b40170/call-PosSortedTaggedBam/1380_IL10R_P176_IGO_10084_4_Aligned.out.sorted.tagged.sorted.bam.bai"
+    # "s3://dp-lab-batch/cromwell-execution/Velopipe2/f8622453-33a1-428b-8eb1-b24c1800214f/call-MergeBam/MPL_CD40.tagged.bai"
+
+    # outPosSortedTaggedBam
+    # "s3://dp-lab-batch/cromwell-execution/Velopipe2/f8622453-33a1-428b-8eb1-b24c1800214f/call-MergeBam/MPL_CD40.tagged.bam"
+
+    # outTagBamLog
+    # "s3://dp-lab-batch/cromwell-execution/Velopipe2/f8622453-33a1-428b-8eb1-b24c1800214f/call-TagBam2/tag_bam.log"
 
     items = list()
 
@@ -32,6 +35,14 @@ def construct_src_dst_info(workflow_id, outputs, base_destination):
 
     items.append(
         (outputs["Velopipe.outPosSortedTaggedBai"], base_destination + "/")
+    )
+
+    items.append(
+        (outputs["Velopipe.outTagBamLog"], base_destination + "/")
+    )
+
+    items.append(
+        (outputs["Velopipe.outVelocytoLog"], base_destination + "/")
     )
 
     return items
